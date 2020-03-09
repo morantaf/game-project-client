f8df1168640b58727a323c65f2a874eedd041d76
manchandajayant <maanjayant@gmail.com>
2020-03-09 13:28:31 +0100

    before pull commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from "react";
 import LoginFormContainer from "./Components/LoginFormContainer";
 import "./App.css";
+import { Route } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         HELLO! THIS IS HOMEPAGE
-        <LoginFormContainer />
+        <Route exact path="/" component={LoginFormContainer} />
       </div>
     );
   }
